e519d34cc154b66b3159d2a10c40b96a74222dc6
ngothithuynhi <momonhi2005@gmail.com>
2025-04-04 16:52:59 +0700

    bài mới đã xong

--- src/main/java/com/mycompany/user/User.java
+++ src/main/java/com/mycompany/user/User.java
@@ -28,4 +28,7 @@ public class User {
     public void updateEmail(String newEmail) {
         this.email = newEmail;
     }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
